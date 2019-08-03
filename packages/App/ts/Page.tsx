a1e37abb4ce4f4c71ee4a4edaad1a9c6953bc384
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2019-08-03 12:07:55 -0700

    Get the initial version rendering.

--- packages/App/ts/Page.tsx
+++ packages/App/ts/Page.tsx
@@ -1,9 +1,46 @@
 import * as React from "react";
 
+import * as Three from "three";
+
 // CSS
 
 // HTML
 
+const perspectiveRenderer = () => {
+  const camera = new Three.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10,
+  );
+  camera.position.z = 1;
+
+  const scene = new Three.Scene();
+
+  const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+  const material = new Three.MeshNormalMaterial();
+
+  const mesh = new Three.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const renderer = new Three.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  return renderer.domElement;
+};
+
+const CanvasHost = (props: { readonly children: HTMLCanvasElement }) => {
+  const id = "CanvasHost";
+  React.useLayoutEffect(() => {
+    const canvasHostDiv = document.getElementById(id);
+    if (canvasHostDiv && canvasHostDiv.parentNode) {
+      canvasHostDiv.parentNode.replaceChild(props.children, canvasHostDiv);
+    }
+  }, []);
+  return <div id={id} />;
+};
+
 export const Page = () => {
-  return <canvas style={{ fill: "black" }} />;
+  const renderer = React.useMemo(perspectiveRenderer, []);
+  return <CanvasHost>{renderer}</CanvasHost>;
 };
--- packages/App/ts/Renderer.tsx
+++ packages/App/ts/Renderer.tsx
@@ -4,6 +4,4 @@ import * as ReactDOM from "react-dom";
 
 import * as App from "App";
 
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(<App.Page />, rootElement);
+ReactDOM.render(<App.Page />, document.getElementById("root"));
